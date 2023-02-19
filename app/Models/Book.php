4b1d9954cda2afec9159ba5fc9238255b691d0ea
NevenaDukic00 <nevenadukic.business@outlook.com>
2023-02-19 17:56:48 +0100

    Tables and Models

---
+++ app/Models/Book.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+    public function selectedBook(Type $var = null)
+    {
+        return $this->hasMany(SelectedBook::class); 
+    }
+    public function genre()
+    {
+        return $this->belongsTo(Genre::class);
+    }
+}
