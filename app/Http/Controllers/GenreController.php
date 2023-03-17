963dd5149fe8fcc8fc5ac75785a3a05b14f39f81
NevenaDukic00 <nevenadukic.business@outlook.com>
2023-03-17 11:19:46 +0100

    Get genres

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -4,18 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Genre;
 use Illuminate\Http\Request;
-use App\Http\Resources\GenreCollection;
+use App\Http\Resources\GenreResource;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 class GenreController extends Controller
 {
     public function index()
     {
-        if ( Auth::user()->email!="admin@gmail.com") {
-            return response()->json(['response' => "You cannot enter a new theatre because you are not an admin!"]);
-        } 
-
+      
         $genres = Genre::all();
-        return new GenreCollection($genres);
+        
+        return GenreResource::collection($genres);
     }
 }
